7f3e226e88d75cfeae906e3d646faedd1babacd0
sethkadin123 <kadin.sit@student.passerellesnumeriques.org>
2024-04-26 19:02:47 +0700

    update airport

--- Airport/Seat.ts
+++ Airport/Seat.ts
@@ -1,10 +1,10 @@
-import { MealType } from "../Enums/MealType";
+
 import { SeatType } from "../Enums/SeatType";
 import { Passenger } from "../Peoples/Passenger";
 
-class Seat{
+export class Seat{
    private type: SeatType
-   private SeatNumber: string
+   private SeatNumber: string   
    private Passenger:Passenger
    constructor(type: SeatType,SeatNumber:string,Passenger:Passenger){
     this.type = type;
