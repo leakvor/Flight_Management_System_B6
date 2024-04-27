f2ec5538452886c6bb657d78b7a4b2535b368331
Leak <vorleak.kun@student.passerellesnumeriques.org>
2024-04-27 14:28:12 +0700

    pull from main

--- Main.ts
+++ Main.ts
@@ -3,6 +3,6 @@ import { Passenger } from "./Peoples/Passenger";
 import { Gender } from "./Enums/Gender";
 
 
-const passenger1 = new Passenger("kadin","sit",23,89,Gender.Female,"46E");
-console.log(passenger1);
+// const passenger1 = new Passenger("kadin","sit",23,89,Gender.Female,"46E");
+// console.log(passenger1);
 
